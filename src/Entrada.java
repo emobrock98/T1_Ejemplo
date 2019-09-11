65bb67030622e71eca9955399464b96ea9b591ff
eddym <bayaxdios@gmail.com>
2019-09-11 18:04:06 +0200

    Initial commit

---
+++ src/Entrada.java
@@ -0,0 +1,6 @@
+public class Entrada {
+    public static void main(String[] args) {
+        System.out.printf("Primer proyecto GitHub");
+        System.out.printf("Segunda línea de código");
+    }
+}
